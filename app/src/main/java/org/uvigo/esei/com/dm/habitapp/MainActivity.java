d4601b890c794b26cac535a4850a000d0bc8737a
alici <alicianoal1@gmail.com>
2024-12-10 22:01:01 +0100

    notificaciones con boton

--- app/src/main/java/org/uvigo/esei/com/dm/habitapp/MainActivity.java
+++ app/src/main/java/org/uvigo/esei/com/dm/habitapp/MainActivity.java
@@ -1,11 +1,20 @@
 package org.uvigo.esei.com.dm.habitapp;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.Manifest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import org.uvigo.esei.com.dm.habitapp.activities.HabitsListActivity;
 import org.uvigo.esei.com.dm.habitapp.activities.LoginActivity;
@@ -16,7 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
     private HabitFacade habitFacade;
 
-    private Button btnLogin, btnRegister;
+    private Button btnLogin, btnRegister, noti;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +43,18 @@ public class MainActivity extends AppCompatActivity {
 
         // Si el usuario no está autenticado, configurar la vista principal
         setContentView(R.layout.activity_main);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                if (checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
+                }
+            }
 
         // Inicializar vistas
         btnLogin = findViewById(R.id.btnLogin);
         btnRegister = findViewById(R.id.btnRegister);
+        noti = findViewById(R.id.noti);
+        noti.setOnClickListener(view ->
+                createNotification() );
 
         // Configurar los listeners para los botones
         btnLogin.setOnClickListener(view -> {
@@ -51,7 +69,24 @@ public class MainActivity extends AppCompatActivity {
 
         // Inicializar el HabitFacade
         habitFacade = new HabitFacade((HabitApplication) getApplication(), this);
+        createNotificationChannel();
+
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1) { // Código de solicitud de POST_NOTIFICATIONS
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permiso concedido
+                Log.d("Permission", "POST_NOTIFICATIONS permission granted");
+            } else {
+                // Permiso denegado
+                Log.e("Permission", "POST_NOTIFICATIONS permission denied");
+            }
+        }
+    }
+
 
     /**
      * Verificar si el usuario ya ha iniciado sesión.
@@ -62,4 +97,37 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences sharedPreferences = getSharedPreferences("Session", MODE_PRIVATE);
         return sharedPreferences.getBoolean("isLogged", false);
     }
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is not in the Support Library.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("1", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this.
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+        private void createNotification(){
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+    
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "1")
+                    .setSmallIcon(android.R.drawable.star_on)
+                    .setContentTitle("My notification")
+                    .setContentText("Much longer text that cannot fit one line...")
+                    .setStyle(new NotificationCompat.BigTextStyle()
+                            .bigText("Much longer text that cannot fit one line..."))
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setContentIntent(pendingIntent) // Asociar el PendingIntent
+
+                    .setAutoCancel(true);
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.notify(1, builder.build());
+        }
 }
